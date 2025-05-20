68a51150878c57499d43b03bb709d1959e2accb9
Pinki Mandal <99741856+pinki-mandal@users.noreply.github.com>
2025-05-20 20:12:50 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,3 @@
-// // import  express from 'express'
-// import  express from "express"
-// import  dotenv  from 'dotenv'
-// const app = express();
-// import   databaseConnection  from "./utils/database.js";
-// import  cookieParser  from "cookie-parser"
-// import userRoutes from './routers/userRoutes.js'
- 
-// dotenv.config(); 
-// const P0RT = process.env.PORT || 8080; 
-// databaseConnection()
-
-
-// app.use(express.json())
-// app.use(cookieParser)
-// app.use(express.urlencoded({extended:true}))
-// app.use("api/v1/user",userRoutes)
-
-// app.get("/",(req,res)=>{
-//     res.status(200).json({
-//         message:"hello pinki mandal",
-//         success:true
-//     })
-// })
-
-
-// app.listen(()=>{
-//     console.log(`server is running on port ${P0RT}`)
-// })
-
-
-
 
 // import express from 'express'
 import express from "express";
